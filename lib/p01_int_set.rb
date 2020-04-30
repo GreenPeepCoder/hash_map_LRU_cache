af11dcc54ad4d76b0d3f8d6d678b04e30f90dd8b
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-04-30 13:56:59 -0400

    First commit for Hash Map & LRU Cache part 1 MaxIntSet completed

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,90 @@
+class MaxIntSet
+ 
+  attr_reader :store
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    is_valid?(num)
+    @store[num] = true
+  end
+  
+  def remove(num)
+    is_valid?(num)
+    @store[num] = false
+  end
+  
+  def include?(num)
+    is_valid?(num)
+    @store[num]
+  end
+  
+  private
+  
+  def is_valid?(num)
+    if num > @store.length || num < 0
+      raise "Out of bounds"
+    end
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
