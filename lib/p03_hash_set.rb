96f1043a9ac3c5edcdb898175a30e980b4721a30
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-05-06 16:37:48 -0400

    Finished Phase 3: HashSet - simple improvement on ResizingIntSet, only this hash can store any data type, not just integers

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_accessor :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,18 +7,30 @@ class HashSet
   end
 
   def insert(key)
+    resize! if count >= num_buckets
+    num = key.hash
+    if self.include?(num) == false
+      @count += 1
+      self[num] << num
+    end
   end
 
   def include?(key)
+    self[key.hash].include?(key.hash)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key.hash].delete(key.hash)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +38,9 @@ class HashSet
   end
 
   def resize!
+    old_store = self.store
+    self.count = 0
+    self.store = Array.new(num_buckets * 2) {Array.new}
+    old_store.flatten.each { |num| insert(num)}
   end
 end
